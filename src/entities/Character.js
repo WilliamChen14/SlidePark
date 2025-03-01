b34959d71720ba6258a0fe3a10d88e1f80284d5a
susanaasun <ss2susana@gmail.com>
2025-03-01 01:51:21 -0800

    Space

--- src/entities/Character.js
+++ src/entities/Character.js
@@ -132,7 +132,6 @@ export class Character {
         const raycaster = new THREE.Raycaster();
         const downVector = new THREE.Vector3(0, -1, 0);
         const origin = currentCharacterPosition;
-
         raycaster.set(origin, downVector);
         const intersections = raycaster.intersectObjects(sceneMeshes, true);
 
